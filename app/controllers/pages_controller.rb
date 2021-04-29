8dcc894efe2f72bfd0fed5e5cc40abd0cf8b9069
andre <andrecabralpires@gmail.com>
2021-04-29 19:28:04 +0100

    finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def scroll_page
+  end
 end
---
+++ app/javascript/components/typed.js
@@ -0,0 +1,15 @@
+import Typed from 'typed.js';
+
+
+const BannerText = () => {
+   new Typed('#typed-text', {
+  strings: ['Javascript', 'It can be annoying!'],
+  typeSpeed: 40,
+  loop:true
+});
+}
+
+export { BannerText }
+
+
+
